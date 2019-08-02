56b80cd67a3ac754ae855a3ccb6fe5446f23798e
treinacloud <52660631+treinacloud@users.noreply.github.com>
2019-08-02 16:00:49 +0100

    Update apache_stop.sh

--- codedeploy/apache_stop.sh
+++ codedeploy/apache_stop.sh
@@ -2,7 +2,6 @@
  
 #Stop no servidor web apache2
 service apache2 stop
-a2enmod headers
 if [[ -f /var/www/cloudcamp-backend ]]; then
     echo "Removendo DIR"
     rm -rf /var/www/cloudcamp-backend
